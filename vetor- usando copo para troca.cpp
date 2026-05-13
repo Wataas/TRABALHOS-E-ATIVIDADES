b384d1ede8dd74f2d9501b2193e23dd0b980b222
Wataas <104842726+Wataas@users.noreply.github.com>
2026-05-13 10:49:35 -0300

    Create vetor- usando copo para troca.cpp

---
+++ vetor- usando copo para troca.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    
+    int vetA[6]={2,3,4,5,2,2};
+    int vetB[6]={7,7,9,7,7,9};
+    int i;
+    int copo;
+    
+    for (i=0; i<6; i++) {
+        
+        copo = vetA[i];
+        vetA[i] = vetB[i];
+        vetB[i] = copo;
+    }
+    
+    cout<<"vetor A: ";
+     for (i=0; i<6; i++) {
+         
+         cout<<vetA[i]<<" ";
+     }
+     
+     cout<< endl;
+     cout<<"vetor B: ";
+     for (i=0; i<6; i++) {
+         
+         cout<<vetB[i]<<" ";
+     }
+}
