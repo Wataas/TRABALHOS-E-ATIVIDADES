d515d40e7436a123f40b1945087b6dd058586099
Wataas <104842726+Wataas@users.noreply.github.com>
2026-05-13 10:51:37 -0300

    Update vetor- usando copo para troca.cpp

--- vetor- usando copo para troca.cpp
+++ vetor- usando copo para troca.cpp
@@ -29,3 +29,39 @@ int main ()
          cout<<vetB[i]<<" ";
      }
 }
+
+
+//VERSAO COM SWAP
+//VERSAO COM SWAP
+//VERSAO COM SWAP
+//VERSAO COM SWAP
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    
+    int vetA[6]={2,3,4,5,2,2};
+    int vetB[6]={7,7,9,7,7,9};
+    int i;
+ 
+    
+    for (i=0; i<6; i++) {
+        
+        swap(vetA[i], vetB[i]);
+    }
+    
+    cout<<"vetor A: ";
+     for (i=0; i<6; i++) {
+         
+         cout<<vetA[i]<<" ";
+     }
+     
+     cout<< endl;
+     cout<<"vetor B: ";
+     for (i=0; i<6; i++) {
+         
+         cout<<vetB[i]<<" ";
+     }
+}
