e9eedbc194ef20e893e8b75c4ae7de45f0fd1262
Wataas <104842726+Wataas@users.noreply.github.com>
2026-05-29 15:43:34 -0300

    Update GENIUS.cpp

--- GENIUS.cpp
+++ GENIUS.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <unistd.h>
+//Usando a biblioteca conio, funciona no codeblocks
+
+
+
+#include <time.h> //para tempo
 using namespace std;
 
 void dc() {cout<<"=========================================="<< endl;}
 void pausa() {cin.get();}
 
 int main () {
+    srand(time(0));
     
+    int sequencia[18];
+    int i=0;
     int escolha;
     int pagina = 1;
+    int velocidade;
+    int tamanho;
+    int limite;
     
     
         while(pagina != 0) {
@@ -33,19 +44,19 @@ int main () {
                 if (escolha==1) {
                     pagina = 4;
                     system("clear");
-                       sleep(1);
+                       sleep(0.6);
                     
                 }
                 else if (escolha==2) {
                     pagina = 2;
                     system("clear");
-                       sleep(1);
+                       sleep(0.6);
                     
                 }
                 else if (escolha==3) {
                     pagina = 3;
                     system("clear");
-                       sleep(1);
+                       sleep(0.6);
                     
                 }
                 else {cout<<"opçao invalida"<< endl;}
@@ -63,7 +74,7 @@ int main () {
                 if (escolha==1) { 
                     pagina = 1;
                     system("clear");
-                       sleep(1);
+                       sleep(0.6);
                     
                 }
                     else { pagina = 2;}
@@ -79,15 +90,76 @@ int main () {
                 cout << "||  [ 1 ]  ======== Noob   ========     ||" << endl;
                 cout << "||  [ 2 ]  ======== Pró    ========     ||" << endl;
                 cout << "||  [ 3 ]  ======== Hacker ========     ||" << endl;
-                cout << "||                                      ||" << endl;
+                cout <<"||  [ 4 ]  ======== voltar ========     ||" << endl;
                 dc();
                 cout << "Escolha uma opcao >> ";
-                cin >> pagina;
+                cin >> escolha;
+                
+                if (escolha==1) {
+                    
+                    limite = 6;
+                    velocidade = 4;
+                    tamanho= 1;
+                    pagina=5;
+                     system("clear");
+                       sleep(0.8);
+                }
+                else if (escolha==2) {
+                    
+                    limite = 9;
+                    velocidade = 50000;
+                    tamanho=3;
+                    pagina = 5;
+                     system("clear");
+                       sleep(0.8);
+                }
+                else if (escolha==3) {
+                    
+                    limite = 18;
+                    velocidade = 0.2;
+                    tamanho=6;
+                    pagina = 5;
+                     system("clear");
+                       sleep(0.8);
+                } 
+                else if (escolha==4) {
+                    
+                    pagina=1;
+                     system("clear");
+                       sleep(0.6);
+                }
+                else { pagina = 4;
+                     system("clear");
+                       sleep(1);
+                }
+                break;
+                
+                case 5:
+                
+            while(i<=limite) {
+                
+                sequencia[i] = rand()%4+1;
+                
+             for(int x=0; x<i; x++) {
+            
+            cout << sequencia[x] << " " << flush;
+            
+              usleep(velocidade);
+                }
+            
+                sleep(2);
+            
+                system("clear");
+            
+                i++;
+            
+            break;
          
                 
                 
-                
             }
+            }
+
         }
         
         
