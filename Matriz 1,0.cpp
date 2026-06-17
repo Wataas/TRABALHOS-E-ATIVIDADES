f0f8603e1ea8e2240d62a2062c44f92fe39a42c9
Wataas <104842726+Wataas@users.noreply.github.com>
2026-06-17 10:49:22 -0300

    Update Matriz 1,0.cpp

--- Matriz 1,0.cpp
+++ Matriz 1,0.cpp
@@ -1,27 +1,36 @@
-#include <iostream>
-using namespace std;
-
-int main() {
+   int soma=0;
     
-    int ma[4][3];
-    int co;
-    int li;
+    cout<<"preencha a matriz A"<< endl;
     
-    for(li=0; li<4; li++) {
-        for(co=0; co<3; co++) {
-            
-            if ((li==1 || li==2) & co==1) {
-                ma[li][co]=0;
-            }
-            else {ma[li][co]=1;
-            }
+    for(li=0; li<3; li++) {
+        for(c=0; c<5; c++) {
+            cin>>maA[li][c];
         }
     }
     
-  for(li=0; li<4; li++) {
-        for(co=0; co<3; co++) {
-            cout<<ma[li][co]<<" ";
+    cout<<"preencha a matriz B"<< endl;
+    
+    for(li=0; li<3; li++) {
+        for(c=0; c<5; c++) {
+            cin>>maB[li][c];
+        }
+    }
+    
+    cout<<"matriz c"<< endl;
+    
+    for(li=0; li<3; li++) {
+        for(c=0; c<5; c++) {
+            
+            maC[li][c] = maA[li][c] + maB[li][c];
+        }
+    }
+    
+     for(li=0; li<3; li++) {
+        for(c=0; c<5; c++) {
+            
+            cout<<maC[li][c]<<" ";
         }
         cout<<endl;
-  }
+     }
 }
+ 
